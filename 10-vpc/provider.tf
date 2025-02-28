673a9b75da6024d5d65803345ca831f6f15b9f87
rajikakani412 <raji.kakani@gmail.com>
2025-02-28 13:33:00 +0530

    updates

--- 10-vpc/provider.tf
+++ 10-vpc/provider.tf
@@ -10,7 +10,9 @@ terraform {
     bucket         = "devops81s-remote-state-dev"
     key            = "expense-dev-vpc"
     region         = "us-east-1"
-    dynamodb_table = "81s-locking-dev"
+    #dynamodb_table = "81s-locking-dev"
+    use_lockfile=true
+  }
   }
 
 }
