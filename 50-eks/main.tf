d9e3012fedb2cb1c2dbb6cd938b46ddc59936d3b
rajikakani412 <raji.kakani@gmail.com>
2025-02-28 15:19:04 +0530

    updates

--- 50-eks/main.tf
+++ 50-eks/main.tf
@@ -1,7 +1,7 @@
 resource "aws_key_pair" "eks" {
   key_name   = "eks"
   # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  #public_key = ""
   public_key = file("~/.ssh/eks.pub")
   # ~ means windows home directory
 }
